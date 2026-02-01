f3164ffa30719563912ea8d2ce2ffa22711ce5d1
RojanShrestha1992 <136217012+RojanShrestha1992@users.noreply.github.com>
2026-02-01 22:49:05 +0545

    get post delete

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-app.get("/", (req,resp)=>{
-    resp.send("helloworld")
+// server start garam
+const app = require("./src/app")
+
+
+
+app.listen(3000, ()=>{
+    console.log("server running on port 3000")
 })
-app.get("/about",(req,resp)=>{
-    resp.send("About page")
-})
-app.listen(3000)
---
+++ src/app.js
@@ -0,0 +1,37 @@
+//server create garam
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+// note ={
+//     title: "first note"
+//     desc: "dfrist desc"
+// }
+
+const notes = []
+   
+app.post('/notes',(req,res)=>{
+// console.log(req.body)
+notes.push(req.body)
+res.status(201).json({
+    message:"note created successfully"
+})
+})
+
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        message:"notes fetced successfully",
+        notes: notes
+    })
+})
+
+
+app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    delete notes[index]
+    res.status(200).json({
+        message:"note deleted"
+    })
+})
+
+module.exports = app
